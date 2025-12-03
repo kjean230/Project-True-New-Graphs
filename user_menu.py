1d8c0b9ce141f8719d00de3654c1bcdb9793002f
Kerwyn Jean <kjean2@fordham.edu>
2025-12-03 10:34:13 -0500

    added more options to the menu to display all possible graphs

--- user_menu.py
+++ user_menu.py
@@ -1,21 +1,26 @@
-# user_menu.py
 """
 Defines the text menu for selecting which graph to display.
 """
 
 GRAPH_MENU = {
-    "1": "Graph 1 (matplotlib): Temperature and AQI over time",
-    "2": "Graph 2 (matplotlib): Spider and fly abundance over time",
-    "3": "Graph 3A (matplotlib): Temperature vs spider abundance (scatter)",
-    "4": "Graph 3B (matplotlib): Temperature vs fly abundance (scatter)",
-    "5": "Graph 3C (matplotlib): AQI vs spider abundance (scatter)",
-    "6": "Graph 3D (matplotlib): AQI vs fly abundance (scatter)",
-    "7": "Graph 1 (seaborn): Temperature and AQI over time",
-    "8": "Graph 2 (seaborn): Spider and fly abundance over time",
-    "9": "Graph 3A (seaborn): Temperature vs spider abundance (scatter)",
+    "1":  "Graph 1 (matplotlib): Temperature and AQI over time",
+    "2":  "Graph 2 (matplotlib): Spider and fly abundance over time",
+    "3":  "Graph 3A (matplotlib): Temperature vs spider abundance (scatter)",
+    "4":  "Graph 3B (matplotlib): Temperature vs fly abundance (scatter)",
+    "5":  "Graph 3C (matplotlib): AQI vs spider abundance (scatter)",
+    "6":  "Graph 3D (matplotlib): AQI vs fly abundance (scatter)",
+
+    "7":  "Graph 1 (seaborn): Temperature and AQI over time",
+    "8":  "Graph 2 (seaborn): Spider and fly abundance over time",
+    "9":  "Graph 3A (seaborn): Temperature vs spider abundance (scatter)",
     "10": "Graph 3B (seaborn): Temperature vs fly abundance (scatter)",
     "11": "Graph 3C (seaborn): AQI vs spider abundance (scatter)",
     "12": "Graph 3D (seaborn): AQI vs fly abundance (scatter)",
+
+    "13": "Graph 4A (seaborn): Spider obs date vs temperature",
+    "14": "Graph 4B (seaborn): Spider obs date vs air quality",
+    "15": "Graph 4C (seaborn): Fly obs date vs temperature",
+    "16": "Graph 4D (seaborn): Fly obs date vs air quality",
 }
 
 
